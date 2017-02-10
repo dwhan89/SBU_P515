6389705344d094d25131840dd049d17a16176caa
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-10 12:44:25 -0500

    Made compatible with python v3 (add parenthesis to print statements)

--- optical/scripts/resonance.py
+++ optical/scripts/resonance.py
@@ -26,8 +26,8 @@ def calc_gf(f, i, j, l):
 gf_87 = calc_gf(f=2, i=1.5, j=0.5, l=0)
 gf_85 = calc_gf(f=3, i=2.5, j=0.5, l=0)
 
-print "gf for Rb85 is %f" % gf_85
-print "gf for Rb87 is %f" % gf_87
+print("gf for Rb85 is %f" % gf_85)
+print("gf for Rb87 is %f" % gf_87)
 
 
 # zeeman splitting energy shift in weak b-field limit with B_ext 
@@ -39,8 +39,8 @@ def delta_E_zee(gf, b_ext):
 del_E_87 = delta_E_zee(gf_87, B_earth)
 del_E_85 = delta_E_zee(gf_85, B_earth)
 
-print "Zeeman Energy shift of Rb85 ground state due to Earth B-field is %f nev." % (del_E_85 * (10 ** 9))
-print "Zeeman Energy shift of Rb87 ground state due to Earth B-field is %f nev." % (del_E_87 * (10 ** 9))
+print("Zeeman Energy shift of Rb85 ground state due to Earth B-field is %f nev." % (del_E_85 * (10 ** 9)))
+print("Zeeman Energy shift of Rb87 ground state due to Earth B-field is %f nev." % (del_E_87 * (10 ** 9)))
 
 
 #convert ev to freq [unit: hz] 
@@ -50,8 +50,8 @@ def ev2freq(ev):
 Rb85_res_freq = ev2freq(del_E_85) 
 Rb87_res_freq = ev2freq(del_E_87)
 
-print "zeeman resonant freq of Rb85 is %f Hz" % Rb85_res_freq
-print "zeeman resonant freq of Rb87 is %f Hz" % Rb87_res_freq
+print("zeeman resonant freq of Rb85 is %f Hz" % Rb85_res_freq)
+print("zeeman resonant freq of Rb87 is %f Hz" % Rb87_res_freq)
 
 
 
