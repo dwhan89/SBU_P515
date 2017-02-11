b71b770ceec857de6835345d998c292f1d504f5a
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-11 14:00:41 -0500

    calculates average and standard deviation of ambient field calculations

---
+++ optical/scripts/ambientBcalc.py
@@ -0,0 +1,17 @@
+import math
+import numpy
+import csv
+import oppumpmagres_funcs as func
+
+ambientB_list = []
+
+with open("../data/ambientFieldmeas.txt",newline='') as csvfile:
+     reader = csv.reader(csvfile, delimiter="	")
+     for row in reader:
+          Ba = float(row[0])*1000000  #in microTesla
+          ambientB_list.append(Ba)
+
+meanBa = numpy.mean(ambientB_list)
+stdBa = numpy.std(ambientB_list)
+
+print("ambient field = %f +/- %f uT" % (meanBa,stdBa))
