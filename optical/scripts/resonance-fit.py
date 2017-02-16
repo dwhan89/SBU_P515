5beb1ba0e13f6a579ba08277b38787fb535c60f4
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-16 12:37:03 -0500

    adding functionality to write (delta E, B_app) for magnetic moment measurements

--- optical/scripts/resonance-fit.py
+++ optical/scripts/resonance-fit.py
@@ -6,12 +6,20 @@ from scipy.optimize import curve_fit
 import oppumpmagres_funcs as func
 
 # for converting V --> frequency (Hz)
-mid_freq = 299e3
-step_freq = 15
+mid_freq = 1390e3
+step_freq = 50
 num_freq = 1000
+#for converting I in maxwell coils to DC B-Field
+I_maxwell = -380.20e-3 # [unit = A]
+
+#choose  isotope
+gf_85 = func.calc_gf(f=3, i=2.5, j=0.5, l=0)
+gf_87 = func.calc_gf(f=2, i=1.5, j=0.5, l=0)
+#gf = gf_85
+gf = gf_87
 
 # for data handling
-datafile = "../data/feb14/rb87/ambientres-87rb-run1.txt"
+datafile = "../data/magmom/rb87/rb87-magmom-run20.txt"
 I_data = []
 f_data = []
 maxI = -99999;
@@ -38,8 +46,10 @@ Inorm_data = [x / maxI for x in I_data]
 popt, pcov = curve_fit(func.lorentzian_function,f_data,Inorm_data,p0=[0.02,mid_freq,10e3])
 perr = np.sqrt(np.diag(pcov))
 x = np.linspace(min(f_data),max(f_data), num=1000)
+res_peak = popt[1]
+res_peak_err = perr[1]
 
-fit_string = 'mean = %.2f +/- %.2f Hz' % (popt[1], perr[1])
+fit_string = 'mean = %.2f +/- %.2f Hz' % (res_peak, res_peak_err)
 
 # plot I/I_0 vs. n 
 plt.scatter(f_data,Inorm_data,label='data')
@@ -49,12 +59,8 @@ plt.ylabel("$I/I_{0}$")
 plt.xlabel("frequency (Hz)")
 plt.text(296000,1.005,fit_string, ha='center', va='center',fontweight='bold')
 
-print("mean = %f Hz" % popt[1])
-print("sigma(mean) = %f Hz" % perr[1])
-
-gf_85 = func.calc_gf(f=3, i=2.5, j=0.5, l=0)
-gf_87 = func.calc_gf(f=2, i=1.5, j=0.5, l=0)
-ambientB = func.B_ext_weak(gf_87, popt[1])
+print("mean = %f +- %f Hz" % (res_peak,res_peak_err))
+ambientB = func.B_ext_weak(gf, res_peak)
 
 print("ambient field = %f T" % ambientB)
 
@@ -65,4 +71,37 @@ print("ambient field = %f T" % ambientB)
 #outFile.write(str(ambientB)+"\n")
 #outFile.close()
 
-plt.show()
+
+
+# ~~~ Everything above for ambient field measurement
+# ~~~ Everything below for magnetic moment measurement
+
+
+
+# got this number from Niv's report. Please double check the number
+N_A = 110    # number of turns in Maxwell Coil A
+N_B = 142    #            ''                   B
+N_C = 110    #            ''                   C
+
+x_A = 0.262     # distance to the plane of the coil B [unit: m]
+x_B = 0.0       #            ''
+x_C = 0.262     #            ''
+
+R_A = 0.591/2.0 # radius of the Maxwell coil A [unit: m]
+R_B = 0.784/2.0 #              ''            B
+R_C = 0.591/2.0 #              ''            C
+
+B_per_I = func.BperI_maxwell(N_A, x_A, R_A, N_B, x_B, R_B, N_C, x_C, R_C) #[unit T/A]
+B_maxwell = B_per_I*I_maxwell
+
+deltaE = func.freq2ev(res_peak)
+print("maxwell field = %f T" % B_maxwell)
+
+#-------------------------------------
+#UNCOMMENT TO PRINT RESULT TO FILE
+#-------------------------------------
+outFile = open("../data/MagMomMeas-Rb87.txt","a")
+outFile.write(str(deltaE)+"	" + str(B_maxwell)  + "\n")
+outFile.close()
+
+#plt.show()
