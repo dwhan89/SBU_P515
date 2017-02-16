3bbfc828039c883fe1676f982792def42ff639f9
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-16 12:42:17 -0500

    minor change -- improvements to plot style

--- optical/scripts/RbXSection.py
+++ optical/scripts/RbXSection.py
@@ -6,12 +6,14 @@ from scipy.optimize import curve_fit
 
 # to calculate number density from temperature
 def numDensity(temp):
-    n = (10**(4.857-4215/temp))/(1.38*(10**-23)*temp)
+    n = 101325.0*(10**(4.857-4215/temp))/(1.38*(10**-23)*temp) #[Pa]
     return n;
 
 def exp(x,a,b,c):
-    #return a*np.exp(-b*x)+c
-    return a*x+b*x**2+c
+    return a*np.exp(-b*x)+c
+
+def linear(x,a,b):
+    return a+b*x
 
 I_data = []
 n_data = []
@@ -33,19 +35,19 @@ with open("../data/cell-warmup-heaton.txt",newline='') as csvfile:
 
 # Normalize intensity
 Inorm_data = [x / maxI for x in I_data]
+Inormlog_data = [math.log(x) for x in Inorm_data]
 
 #fit the data
-popt, pcov = curve_fit(exp,n_data,Inorm_data)
+popt, pcov = curve_fit(linear,n_data,Inormlog_data)
 x = np.linspace(min(n_data),max(n_data), num=1000)
+perr = np.sqrt(np.diag(pcov))
 
 # plot I/I_0 vs. n 
-plt.scatter(n_data,Inorm_data)
-plt.plot(x,exp(x,*popt))
-plt.ylabel("$I/I_{0}$")
-plt.xlabel("Number Density")
-#plt.yscale('log')
-# fit to exponential
+plt.scatter(n_data,Inormlog_data)
+plt.plot(x,linear(x,*popt))
+plt.ylabel("$\ln(I/I_{0})$",size=18,fontweight='bold')
+plt.xlabel("Number Density [$m^{-3}$]",size=18)
+plt.text(2.3e16,-0.1,r'slope = %.2f $\pm$ %.2f $\times10^{-18}$ m$^{3}$' % (popt[1]*1.0e18,perr[1]*1.0e18), ha='center', va='center',fontweight='bold')
+plt.text(2.3e16,-0.11,r'$\sigma$ = %.2f $\pm$ %.2f $\times10^{-17}$ m$^{2}$' % ((popt[1]/0.05)*-1.0e17,(perr[1]/0.05)*1.0e17), ha='center', va='center',fontweight='bold')
 
 plt.show()
-
-print(popt)
