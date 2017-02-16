ed16953e5b640a6889173549bca02034d3d8d15f
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-15 21:52:34 -0500

    print peak position from fit onto plot

--- optical/scripts/resonance-fit.py
+++ optical/scripts/resonance-fit.py
@@ -11,7 +11,7 @@ step_freq = 15
 num_freq = 1000
 
 # for data handling
-datafile = "../data/feb14/rb87/ambientres-87rb-run20.txt"
+datafile = "../data/feb14/rb87/ambientres-87rb-run1.txt"
 I_data = []
 f_data = []
 maxI = -99999;
@@ -39,12 +39,15 @@ popt, pcov = curve_fit(func.lorentzian_function,f_data,Inorm_data,p0=[0.02,mid_f
 perr = np.sqrt(np.diag(pcov))
 x = np.linspace(min(f_data),max(f_data), num=1000)
 
+fit_string = 'mean = %.2f +/- %.2f Hz' % (popt[1], perr[1])
+
 # plot I/I_0 vs. n 
 plt.scatter(f_data,Inorm_data,label='data')
 plt.plot(x,func.lorentzian_function(x,*popt),'r-',label='Lorentzian fit')
 plt.legend()
 plt.ylabel("$I/I_{0}$")
 plt.xlabel("frequency (Hz)")
+plt.text(296000,1.005,fit_string, ha='center', va='center',fontweight='bold')
 
 print("mean = %f Hz" % popt[1])
 print("sigma(mean) = %f Hz" % perr[1])
@@ -54,8 +57,12 @@ gf_87 = func.calc_gf(f=2, i=1.5, j=0.5, l=0)
 ambientB = func.B_ext_weak(gf_87, popt[1])
 
 print("ambient field = %f T" % ambientB)
-outFile = open("../data/RB87ambientFieldmeas.txt","a")
-outFile.write(str(ambientB)+"\n")
-outFile.close()
 
-#plt.show()
+#-------------------------------------
+#UNCOMMENT TO PRINT RESULT TO FILE
+#-------------------------------------
+#outFile = open("../data/RB87ambientFieldmeas.txt","a")
+#outFile.write(str(ambientB)+"\n")
+#outFile.close()
+
+plt.show()
