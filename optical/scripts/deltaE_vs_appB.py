d029906e1ecc167775845c35f5f51d2646e00c53
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-21 15:12:28 -0500

    minor change

--- optical/scripts/deltaE_vs_appB.py
+++ optical/scripts/deltaE_vs_appB.py
@@ -39,7 +39,7 @@ plt.axhline(y=0, color='k')
 plt.grid(True, which='both')
 plt.xticks(fontsize=16)
 plt.yticks(fontsize=16)
-plt.ylabel(r"$\Delta$\textbf{E [neV]}",size=22,fontweight='bold')
+plt.ylabel(r"$\Delta E$ $[neV]$",size=22,fontweight='bold')
 plt.xlabel(r"$B_{app}$ $[\mu T]$",size=22,fontweight='bold')
 plt.margins(0.05)
 
