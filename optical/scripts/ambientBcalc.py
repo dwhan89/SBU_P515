2bfa3325ac7fadb7ea26a14a0a37e1809c57996e
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-21 13:48:42 -0500

    include analysis of energy splitting distributions in addition to B-field distributions

--- optical/scripts/ambientBcalc.py
+++ optical/scripts/ambientBcalc.py
@@ -5,13 +5,16 @@ import oppumpmagres_funcs as func
 
 ambientB_list = []
 
-with open("../data/feb14/rb87/RB87ambientFieldmeas.txt",newline='') as csvfile:
+#with open("../data/feb14/rb87/RB87ambientFieldmeas.txt",newline='') as csvfile:
+with open("../data/feb14/rb85/RB85-deltaEDist.txt",newline='') as csvfile:
      reader = csv.reader(csvfile, delimiter="	")
      for row in reader:
-          Ba = float(row[0])*1000000  #in microTesla
+          #Ba = float(row[0])*1000000  #in microTesla
+          Ba = float(row[0])*1.0e9  #in neV
           ambientB_list.append(Ba)
 
 meanBa = numpy.mean(ambientB_list)
 stdBa = numpy.std(ambientB_list)
 
-print("ambient field = %f +/- %f uT" % (meanBa,stdBa))
+#print("ambient field = %f +/- %f uT" % (meanBa,stdBa))
+print("energy splitting = %f +/- %f neV" % (meanBa,stdBa))
