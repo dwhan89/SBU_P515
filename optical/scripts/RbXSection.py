af470b883fd9794a123fac51c8ebe562b7642240
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-22 23:47:38 -0500

    minor formatting changes to plot

--- optical/scripts/RbXSection.py
+++ optical/scripts/RbXSection.py
@@ -38,7 +38,7 @@ with open("../data/cell-warmup-heaton.txt",newline='') as csvfile:
           t = float(row[2]) + 273.15
           n = numDensity(t)
           sig_n = dndT(t)
-          sig_n = 0.1*sig_n   # sig_t = 0.1 K
+          sig_n = 0.25*sig_n   # sig_t = 0.25 K
           I_data.append(I)
           n_data.append(n)
           n_err.append(sig_n)
@@ -57,12 +57,12 @@ plt.gcf().subplots_adjust(bottom=0.1)
 plt.gcf().subplots_adjust(left=0.2)
 
 # plot I/I_0 vs. n 
-plt.errorbar(n_data,Inormlog_data,xerr=n_err,label='data')
+plt.errorbar(n_data,Inormlog_data,xerr=n_err,color='black',fmt='.',label='data')
 plt.plot(x,linear(x,*popt),label='fit')
-plt.ylabel(r"$\ln(\frac{I}{I_{0}})$",rotation=0,size=18,labelpad=25,fontweight='bold')
+plt.ylabel(r"$\ln{\left(\frac{I}{I_{0}}\right)}$",rotation=0,size=18,labelpad=25,fontweight='bold')
 plt.xlabel(r"\textbf{Number Density} $[m^{-3}]$",size=18)
-plt.text(2.05e16,-0.075,r'\textbf{slope = %.2f $\pm$ %.2f} $\times10^{-18}$ $m^{3}$' % (popt[1]*1.0e18,perr[1]*1.0e18), ha='center', va='center',fontsize=16,fontweight='bold')
-plt.text(2.05e16,-0.085,r'\textbf{$\sigma$ = %.2f $\pm$ %.2f} $\times10^{-17}$ $m^{2}$' % ((popt[1]/0.05)*-1.0e17,(perr[1]/0.05)*1.0e17),ha='center',va='center',fontsize=16,fontweight='bold')
+plt.text(2.25e16,-0.075,r'\textbf{slope = %.2f $\pm$ %.2f} $\times10^{-18}$ $m^{3}$' % (popt[1]*1.0e18,perr[1]*1.0e18), ha='center', va='center',fontsize=16,fontweight='bold')
+plt.text(2.25e16,-0.085,r'\textbf{$\sigma$ = %.2f $\pm$ %.2f} $\times10^{-17}$ $m^{2}$' % ((popt[1]/0.05)*-1.0e17,(perr[1]/0.05)*1.0e17),ha='center',va='center',fontsize=16,fontweight='bold')
 plt.legend()
 
 plt.show()
