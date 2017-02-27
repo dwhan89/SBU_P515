06b6fbb4fbfcc9b90fd250cc0435367e9e24590b
Kevin Wood <kevin.wood@stonybrook.edu>
2017-02-27 17:53:02 -0500

    minor changes

--- optical/scripts/ambientBcalc.py
+++ optical/scripts/ambientBcalc.py
@@ -5,17 +5,17 @@ import oppumpmagres_funcs as func
 
 ambientB_list = []
 
-#with open("../data/feb14/rb87/RB87ambientFieldmeas.txt",newline='') as csvfile:
+with open("../data/feb14/rb85/RB85ambientFieldmeas.txt",newline='') as csvfile:
 #with open("../data/feb14/rb85/RB85-deltaEDist.txt",newline='') as csvfile:
-with open("../data/4v_err/rb85/highBsigma.txt",newline='') as csvfile:
+#with open("../data/4v_err/rb85/highBsigma.txt",newline='') as csvfile:
      reader = csv.reader(csvfile, delimiter="	")
      for row in reader:
-          #Ba = float(row[0])*1000000  #in microTesla
-          Ba = float(row[0])*1.0e9  #in neV
+          Ba = float(row[0])*1000000  #in microTesla
+          #Ba = float(row[0])*1.0e9  #in neV
           ambientB_list.append(Ba)
 
 meanBa = numpy.mean(ambientB_list)
 stdBa = numpy.std(ambientB_list)
 
-#print("ambient field = %f +/- %f uT" % (meanBa,stdBa))
-print("energy splitting = %f +/- %f neV" % (meanBa,stdBa))
+print("ambient field = %f +/- %f uT" % (meanBa,stdBa))
+#print("energy splitting = %f +/- %f neV" % (meanBa,stdBa))
