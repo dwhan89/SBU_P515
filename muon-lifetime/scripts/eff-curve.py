54f110fcbe6f8ba24730fe6fa9808a9c4acf0918
Kevin Wood <kevin.wood@stonybrook.edu>
2017-03-02 18:45:34 -0500

    show S2 and S3 efficiency curves

--- muon-lifetime/scripts/eff-curve.py
+++ muon-lifetime/scripts/eff-curve.py
@@ -4,20 +4,33 @@ import matplotlib.pyplot as plt
 import csv
 from scipy.optimize import curve_fit
 
-datafile = "../data/eff/S2-eff.txt"
-eff_list = []
-HV_list  = []
+datafile2 = "../data/eff/S2-eff.txt"
+eff2_list = []
+HV2_list  = []
+datafile3 = "../data/eff/S3-eff.txt"
+eff3_list = []
+HV3_list  = []
 
-with open(datafile,newline='') as csvfile:
+with open(datafile2,newline='') as csvfile:
          reader = csv.reader(csvfile, delimiter="	")
          for row in reader:
          # if row[1] and row[2] exist...?
-              eff = float(row[0])
-              HV  = abs(float(row[1]))
-              eff_list.append(eff)
-              HV_list.append(HV)
+              eff2 = float(row[0])
+              HV2  = abs(float(row[1]))
+              eff2_list.append(eff2)
+              HV2_list.append(HV2)
 
-plt.scatter(HV_list,eff_list)
+with open(datafile3,newline='') as csvfile:
+         reader = csv.reader(csvfile, delimiter="	")
+         for row in reader:
+         # if row[1] and row[2] exist...?
+              eff3 = float(row[0])
+              HV3  = abs(float(row[1]))
+              eff3_list.append(eff3)
+              HV3_list.append(HV3)
+
+plt.scatter(HV2_list,eff2_list,color='red')
+plt.scatter(HV3_list,eff3_list,color='blue')
 
 plt.show()
 
