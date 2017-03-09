b87b30c3336a859f35acde9a33490660a49c780f
dw <dwhan89@gmail.com>
2017-03-09 11:12:13 -0500

    photos of muon decay experiment setup

--- muon-lifetime/scripts/timecal.py
+++ muon-lifetime/scripts/timecal.py
@@ -1,14 +1,26 @@
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
+import util as u
 
 # data setting
-input_file = "../data/time_cal/time_calibration_mar7.txt"
+run_date   = "mar7"
+run_number = 2
+
+input_tmp  = "../data/time_cal/time_calibration_{}_run{}.txt"
+
+input_file = u.format_string(input_tmp, run_date, run_number)
 
 # plot setting
-#plt.style.use('ggplot')
+lbin = 6
 
+# calibration setting
+period     = 2.96  # unit: us
+num_period = 3     # the number of full periods in the data set      
+
+# read in the data file
 num_row = 0 
+print("reading ", input_file)
 with open(input_file, newline='') as handle:
     reader = csv.reader(handle, delimiter="=")
     
@@ -24,17 +36,43 @@ with open(input_file, newline='') as handle:
     # store the data
     i = 0
     for row in reader:
-        data[1][i] = float(row[0])
+        data[1][i] = int(row[0])
+        print(i, data[1][i])
         i += 1
 
-plt.figure()
-ax = plt.gca()
+'''
+ctr = 0   # count number of period
+i   = lbin
 
-lbin = 0
-plt.plot(data[0][lbin:], data[1][lbin:])
-ax.fill_between(data[0][lbin:], data[1][lbin:], 0)
-ax.set_ylim([0,1000])
+prev_num = 0
+cur_num  = 0
 
-plt.savefig("test.png")
+sig_start = -1
+sig_end   = -1
+while(ctr < num_period):
+    #print(ctr, i, cur_num, sig_start, sig_end)
+    cur_num = data[1][i]
 
+    if(prev_num == 0 and cur_num > 0):
+        print("mark ", i)
+        if(sig_start < 0):
+            sig_start = i
+        else:
+            ctr +=1
+    else:
+        pass
+
+    prev_num = cur_num
+    i += 1
+
+sig_end = i-1
+time_per_bin = (period * num_period) / ((sig_end - sig_start) * 1.0)
+
+
+print("The number of full periods in the data : ", num_period)
+print("period                                 : ", period, " us")
+print("the signal starts                      : ", sig_start)
+print("the signal ends                        : ", sig_end)
+print("time per bin                           : ", time_per_bin, " us/bin")
+'''
 
